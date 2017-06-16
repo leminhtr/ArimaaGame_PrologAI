24db61c9d5eca39345b8264caf79069ddd2d6220
Minh Tri <trileminh16@gmail.com>
2017-06-17 00:45:05 +0200

    gamestate

--- ArimaaIA02/apps/arimaa/projet.pl
+++ ArimaaIA02/apps/arimaa/projet.pl
@@ -1,6 +1,7 @@
 
 :- dynamic board/1.
 
+gamestate(silver, [], gold, []).
 board([[0,0,rabbit,silver],[0,1,rabbit,silver],[0,2,horse,silver],[0,3,rabbit,silver],[0,4,elephant,silver],[0,5,rabbit,silver],[0,6,rabbit,silver],[0,7,rabbit,silver],[1,0,camel,silver],[1,1,cat,silver],[1,2,rabbit,silver],[1,3,dog,silver],[1,4,rabbit,silver],[4,5,horse,silver],[4,6,dog,gold],
 [1,7,cat,silver],[2,7,rabbit,gold],[6,1,horse,gold],[6,2,camel,silver],[6,3,elephant,gold],[6,4,rabbit,gold],[6,5,dog,gold],[6,6,rabbit,gold],[7,0,rabbit,gold],[7,1,rabbit,gold],[7,2,rabbit,gold],[7,3,cat,gold],[7,4,dog,gold],[7,5,rabbit,gold],[7,6,horse,gold],[7,7,rabbit,gold]]).
 
